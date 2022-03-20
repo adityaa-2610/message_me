222376aed0297e43fa110f2e0980fd2f37b4a412
Aditya Agarwal <aditya.agarwal@anchanto.com>
2022-03-21 03:10:50 +0530

    Authenication and  template setup done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+
+// $(document).on('turbolinks:load',function() {
+//     $('.ui.dropdown').dropdown();
+//     $('.message .close').on('click', function() {
+//         $(this).closest('.message').transition('fade');
+//       });
+// })
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# User.create(username: "Aditya", password: "password")
+# User.create(username: "Vivek", password: "password")
+# User.create(username: "Shayshank", password: "password")
+# User.create(username: "Ashish", password: "password")
+# User.create(username: "Sayantan", password: "password")
+
+Message.create(body: "I am a new message" ,user: User.last)
+Message.create(body: "I am a Aditya" ,user: User.first)
